fac849d746226164e6592ad3d04e599cce2d4708
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-13 19:47:07 +0200

    Paths de las rutas faltantes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 // localhost:4200/heroes/
 const routes: Routes = [
@@ -10,6 +13,11 @@ const routes: Routes = [
     component: LayoutPageComponent, // Mirar que importa ./pages/layout-page el de heroes
     children: [
       { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: HeroPageComponent }, // Cuidado con esta ruta, es un comodin, dejar la última
+      { path: '**', redirectTo: 'list' }, // Va a entrar la primera vez que entremos en este componente, la ruta estará vacio
     ]
   },
 ];
