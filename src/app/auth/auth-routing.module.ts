824d51a26354f53b12793943091ab1e6d855b937
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-13 19:27:07 +0200

    Rutas hijas y LazyLoad

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+// localhost:4200/auth/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent // Mirar que importa ./pages/layout-page el de auth
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
-const routes: Routes = [];
+// localhost:4200/heroes/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent, // Mirar que importa ./pages/layout-page el de heroes
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
