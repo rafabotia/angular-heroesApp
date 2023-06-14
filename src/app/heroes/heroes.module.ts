d7ed67ee3fb6691bc8d98b05b2eb8abd6d850361
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-14 18:18:56 +0200

    Material Sidenav, Toolbar e Iconos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
@@ -19,7 +20,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
