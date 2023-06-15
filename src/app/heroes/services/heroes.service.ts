34d6b28894b261349286a69dd55db64034722520
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-15 19:41:24 +0200

    Obtener Héroe por URL- 2

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments.prod';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class HeroesService {
@@ -15,5 +15,12 @@ export class HeroesService {
 
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
 
+  }
+
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      )
   }
 }
