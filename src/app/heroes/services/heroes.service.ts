a64bca70220fc4ccd9ecc9e0a76569967e7128f8
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-15 20:15:44 +0200

    Angular Material - Autocomplete

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -23,4 +23,9 @@ export class HeroesService {
         catchError( error => of(undefined) )
       )
   }
+
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`/heroes?q=${ query }&_limit=6`);
+  }
+
 }
