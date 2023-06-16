9b3f9d9cf8ae3c7fbcca55621df69c9847990be7
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-16 08:42:49 +0200

    Agregar Héroe - HTML

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public publishers = [
+    { id: 'DC Comics', value: 'DC - Comics' },
+    { id: 'Marvel Comics', value: 'Marvel - Comics' },
+  ];
+
 }
